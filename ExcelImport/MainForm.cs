ab191b9e60080145e81bbc5cbd9c18544dd66d60
Darko Milutinović <dmilutinovic@gmail.com>
2015-02-08 22:07:03 +0100

    Added ManagedDTS support

--- ExcelImport/MainForm.cs
+++ ExcelImport/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.SqlServer.Dts.Runtime;
 
 namespace ExcelImport
 {
@@ -33,7 +34,18 @@ namespace ExcelImport
 
 		private void btnLoad_Click(object sender, EventArgs e)
 		{
-			MessageBox.Show("Podaci su uspešno upisani u bazu.");
+			string packageLocation = @"D:\Temp\Packages\ExcelYouth.dtsx";
+
+			PackageEvents pe = new PackageEvents();
+
+			Microsoft.SqlServer.Dts.Runtime.Application app = new Microsoft.SqlServer.Dts.Runtime.Application();
+			Package package = app.LoadPackage(packageLocation, pe);
+			//package.PackagePassword = "hitachi";
+			package.Variables["User::ExcelFilePath"].Value = this.txtPath.Text;
+
+			DTSExecResult result = package.Execute();
+
+			//MessageBox.Show(result.ToString());
 		}
 	}
 }
---
+++ ExcelImport/PackageEvents.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.SqlServer.Dts.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelImport
+{
+	public class PackageEvents : IDTSEvents
+	{
+		public void OnBreakpointHit(IDTSBreakpointSite breakpointSite, BreakpointTarget breakpointTarget)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnCustomEvent(TaskHost taskHost, string eventName, string eventText, ref object[] arguments, string subComponent, ref bool fireAgain)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool OnError(DtsObject source, int errorCode, string subComponent, string description, string helpFile, int helpContext, string idofInterfaceWithError)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnExecutionStatusChanged(Executable exec, DTSExecStatus newStatus, ref bool fireAgain)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnInformation(DtsObject source, int informationCode, string subComponent, string description, string helpFile, int helpContext, string idofInterfaceWithError, ref bool fireAgain)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnPostExecute(Executable exec, ref bool fireAgain)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnPostValidate(Executable exec, ref bool fireAgain)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnPreExecute(Executable exec, ref bool fireAgain)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnPreValidate(Executable exec, ref bool fireAgain)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnProgress(TaskHost taskHost, string progressDescription, int percentComplete, int progressCountLow, int progressCountHigh, string subComponent, ref bool fireAgain)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool OnQueryCancel()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnTaskFailed(TaskHost taskHost)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnVariableValueChanged(DtsContainer DtsContainer, Variable variable, ref bool fireAgain)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void OnWarning(DtsObject source, int warningCode, string subComponent, string description, string helpFile, int helpContext, string idofInterfaceWithError)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
